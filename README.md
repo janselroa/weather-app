3d33d8e9bf80b63e6ea6af8d89472e90479c2ffb
Jansel <janselroa2424@gmail.com>
2022-04-09 12:17:53 -0400

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # weather app
 ## Preview
-<img src="./preview.png">
+<img src="./preview1.png">
 <img src="./preview2.png">
 
 ## Download
@@ -8,3 +8,5 @@
 git clone https://github.com/janselroa/weather-app.git
 ```
 then open the index.html file and you can use the app
+watch live on https://wondrous-cheesecake-e6a2f6.netlify.app/
+
--- index.js
+++ index.js
@@ -1,54 +1,59 @@
 const input = document.querySelector("input")
 const title = document.querySelector('.title')
 const content = document.querySelector('.content')
+const btn = document.querySelector('button')
 const error = document.querySelector('.error')
 const texts = ["Quieres saber que clima habra hoy","Ve el clima de cualquier cuidad","Hoy estara soleado?"]
 setInterval(()=>{
     title.classList.toggle('animation-text')
     title.textContent= texts[Math.floor(Math.random() * (3 - 0)) + 0]
 },5000)
-const url = "http://api.weatherstack.com/current"
-const api_key = '2bad2d4649e64b34d3416d70d327c255'
+const url = "https://api.weatherbit.io/v2.0/current"
+const api_key = '973a5a5f6ace4a21b2b30ca14dd5ca07'
 
 const background = {
-    "Sunny":"url('./assets/sunny.webp')",
-    "Partly cloudy":"url('./assets/clouds.webp')",
-    "Clear":"url('./assets/clear.jpg')",
-    "Moderate rain":"url('./assets/rain.jpg')",
-    "Light Rain":"url('./assets/rain.jpg')",
-    "Light rain showe":"url('./assets/rain.jpg')"
+    "Cielo despejado":"sunny.webp",
+    "Nubes dispersas":"clouds.webp",
+    "Cubierto":"clouds.webp",
+    "Clear":"clear.jpg",
+    "Poco nuboso":"clouds.webp",
+    "Nuboso":"nuboso.jpg",
+    "Poco nuboso":"nuboso.jpg",
+    "Lluvia":"lluvia.webp",
+    "Lluvia ligera":"rain.jpg",
 }
-const weather_descriptions = {
-    "Sunny":"Dia soleado",
-    "Partly cloudy":"Un parde nubes",
-    "Clear":"Cielo claro",
-    "Patchy rain possible":"Con posibilidad de lluvia",
-    "Moderate rain":"lluvia moderada",
-    "Light rain showe":"Lluvia ligera"
-}
-input.addEventListener('keydown',(e)=>{
-    if(e.key==="Enter"){
-        fetch(`${url}?access_key=${api_key}&query=${input.value}`)
+const getClimate = ()=>{
+    fetch(`${url}?key=${api_key}&city=${input.value}&lang=es`)
         .then(data=>data.json())
         .then(data=>{
-            document.body.style.backgroundImage=background[data.current.weather_descriptions[0]]
+            data=data.data[0]
+            error.style.display="none"
+            document.body.style.backgroundImage=`url('./assets/${background[data.weather.description]}')`
             document.body.style.backgroundColor = "rgba(17, 17, 17, 0.356)"
             content.innerHTML=`
-                <h2>${data.request.query}</h2>
+                <h2>${data.city_name}</h2>
                 <div>
-                <img src=${data.current.weather_icons[0]}>
-                <p class="temp">${data.current.temperature}°c</p>
-                <p class="description">${weather_descriptions[data.current.weather_descriptions[0]]}</p>
-                <p class="time">${data.location.localtime}</p>
+                <img src='https://www.weatherbit.io/static/img/icons/${data.weather.icon}.png'>
+                <p class="temp">${data.temp}°c</p>
+                <p class="time">${data.datetime}</p>
+                <p class="description">${data.weather.description}</p>
                 <div class="wind">
-                <p>Direccion del viento: ${data.current.wind_dir}</p>
-                <p>Grado de viento: ${data.current.wind_degree}</p>
-                <p>Velocidad del viento: ${data.current.wind_speed}</p>
+                <p>Cantidad estimada de nubes: ${data.clouds}</p>
+                <p>Direccion del viento: ${data.wind_cdir_full}</p>
+                <p>Velocidad del viento: ${data.wind_spd}</p>
                 </div>
                 </div>
                 `
         }).catch(err=>{
+            error.style.display="inline"
             error.textContent=`No se a encontrado "${input.value}"`
         })
+}
+input.addEventListener('keydown',(e)=>{
+    if(e.key==="Enter"){
+        getClimate()
     }
+})
+btn.addEventListener('click',()=>{
+    getClimate()
 })
